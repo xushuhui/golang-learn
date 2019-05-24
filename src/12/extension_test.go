9d3df219929572992ce3683bba147bca37229e48
“xushuhui <474497097@qq.com>
2019-05-24 17:35:37 +0800

    fix

---
+++ src/12/extension_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+type Pet struct {
+}
+
+func (p *Pet) Speak() {
+	fmt.Print("...")
+}
+func (p *Pet) SpeakTo(host string) {
+	p.Speak()
+	fmt.Println(" ", host)
+}
+
+type Dog struct {
+	p *Pet
+}
+
+func (d *Dog) Speak() {
+	d.p.Speak()
+}
+func (d *Dog) SpeakTo(host string) {
+	d.p.SpeakTo(host)
+}
+func TestDog(t *testing.T) {
+
+}
